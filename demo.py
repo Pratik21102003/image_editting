145a594e144c2687a338c1500f0da32ca8862acb
Pratik Tiwari <113763709+Pratik21102003@users.noreply.github.com>
2024-03-09 09:37:12 +0530

    Update and rename app.py to demo.py

--- demo.py
+++ demo.py
@@ -1,19 +1,21 @@
-import streamlit as st
-import numpy as np 
-import pandas as pd
 import cv2
-img=st.file_uploader("Upload an image")
+import streamlit as st
+import numpy as np
+from PIL import Image
+image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
 option=st.sidebar.selectbox('Select one',(None,'Flip1','Flip2','Flip3'))
 option1=st.sidebar.selectbox('Select one',(None,'opt1','opt2','opt3','opt4','opt5','opt6','opt7'))
-if img:
+opt=st.sidebar.slider("Enhance",0.0,10.0,0.1)
+opt1=st.sidebar.slider("Brightness",min_value=-50, max_value=50, value=0)
+opt2=st.sidebar.slider("Blur",0.5,3.5)
+if image_file is not None:
     col1,col2=st.columns(2)
     with col1:
-        image=cv2.imread(img.name)
-        img1=image.copy()
-        st.image(img) 
-                          
+         original_image = Image.open(image_file)
+         original_image = np.array(original_image)
+         st.image(original_image)
+         img2=original_image.copy()
     with col2:
-        img2 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)  # Convert to RGB format because opencv BGR leta hai or streamlit RGB
         if option==None:
             pass
         if option=='Flip1':
@@ -38,9 +40,12 @@ if img:
             img2[:,:,0:3:2]=0
         elif option1 == 'opt7':
             img2 =cv2.cvtColor(img2,cv2.COLOR_RGB2BGR)  
-        opt=st.sidebar.slider("Enhance",0.0,10.0,0.1)
         if opt:
             img2=cv2.detailEnhance(img2,sigma_s=opt)  
+        if opt1:
+            img2 = cv2.convertScaleAbs(img2, beta=opt1)
+        if opt2:
+           img2= cv2.GaussianBlur(img2, (11, 11),opt2)
         st.image(img2)
         btn=st.button('Download')
         if btn: 
